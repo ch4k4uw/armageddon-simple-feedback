b1af733aa9db5d5d40e6f67f985fabea0699f4e1
ch4k4uw <garra_e@hotmail.com>
2022-12-06 01:52:30 -0300

    Finished jest-ts-auto-mock configuration

--- config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const extension_1 = require("ts-auto-mock/extension");
-extension_1.Provider.instance.provideMethodWithDeferredValue((_name, value) => {
-    return jest.fn().mockImplementation(value);
-});
