4141f6cb10d0d567db4ef38502f2d077df9b5bbe
Pedro Motta <garra_e@hotmail.com>
2023-05-29 21:29:59 -0300

    Added the  field into feedback results

--- src/domain/feedback/entity/feedback.ts
+++ src/domain/feedback/entity/feedback.ts
@@ -4,6 +4,7 @@ export class Feedback {
         public readonly topic: string = "",
         public readonly rating: number = 0,
         public readonly reason: string = "",
+        public readonly created: number = 0,
     ) { }
 
     static readonly empty = new Feedback();
