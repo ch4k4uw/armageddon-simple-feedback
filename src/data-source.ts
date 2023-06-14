e47b305f0c0a5cab9ab47c5291305631f83bbc9e
Pedro Motta <garra_e@hotmail.com>
2023-06-14 14:33:43 -0300

    Changed the database path and some seed data

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,9 +3,11 @@ import { SeederOptions } from "typeorm-extension";
 import { DefaultSeeder } from "./infra/database/orm/seeds/default-seeder";
 import { root } from "./root-path";
 
+const dbPath = process.env.ARMAGEDDON_DB_PATH || `${root}/data/db.sqlite`;
+
 const dsOptions: DataSourceOptions & SeederOptions = {
     type: "sqlite",
-    database: `${root}/data/db.sqlite`,
+    database: dbPath,
     logging: true,
     entities: [`${__dirname}/infra/database/orm/*.entity.@(ts|js)`],
     migrations: [`${__dirname}/infra/database/orm/migrations/*.@(ts|js)`],
